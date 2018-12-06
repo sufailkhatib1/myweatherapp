2035cf022d88c6b5654c6ba4744a6936a0b9e88d
sufail khatib <sufailkhatib@icloud.com>
2018-12-06 10:10:27 +0200

    my weather app

---
+++ weather/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Sufail Khatib on 05/12/18.
+//  Copyright © 2018 Sufail Khatib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
